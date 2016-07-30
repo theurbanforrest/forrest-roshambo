f9447c8535d3f80c9eeecc7097d5f2b4cf213ce4
Forrest Ching <theurbanforrest@Forrests-Air.home>
2016-07-30 18:34:51 -0400

    added icon assets, built out StartMatchViewController

--- forrest-roshambo/StartMatchViewController.swift
+++ forrest-roshambo/StartMatchViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  StartMatchViewController.swift
 //  forrest-roshambo
 //
 //  Created by Forrest Ching on 7/30/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class StartMatchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
